f6f59e5a5c1652b6c5b515d8d64e66b0e190669a
sallyysamehh <samehsaied266@gmail.com>
2025-03-15 06:43:32 +0200

    // add Sally

--- src/test/java/ExerciseTest.java
+++ src/test/java/ExerciseTest.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.w3c.dom.html.HTMLInputElement;
+
+import javax.swing.*;
 
 public class ExerciseTest {
 
@@ -32,7 +35,23 @@ public class ExerciseTest {
             WebElement signupBtn = driver.findElement(By.xpath("//button[text()='Signup']"));
             signupBtn.click();
         }
+        @Test
+        public void login(){
+        WebElement signupLoginBtn = driver.findElement(By.linkText("Signup / Login"));
+        signupLoginBtn.click();
 
+        WebElement email = driver.findElement(By.xpath("//input[@data-qa='login-email']"));
+        email.sendKeys("sallysameh2002@icloud.com");
+
+        WebElement password = driver.findElement(By.xpath("//input[@data-qa='login-password']"));
+        password.sendKeys("sally@542002");
+
+        WebElement loginBtn = driver.findElement(By.xpath("//button[text()='Login']"));
+        loginBtn.click();
+
+        WebElement loggedInAs =driver.findElement(By.xpath("//a[text()=' Logged in as ']"));
+
+        }
 
         @AfterMethod
         public void closeBrowser(){
