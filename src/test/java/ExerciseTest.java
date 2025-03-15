1857f6c6180a9d62c5391a2bc5d1b2d4efb56a81
menna <mennaihme@gmail.com>
2025-03-15 05:14:22 +0200

    update

--- src/test/java/ExerciseTest.java
+++ src/test/java/ExerciseTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -18,19 +19,41 @@ public class ExerciseTest {
         driver.manage().window().maximize();
         }
 
-        @Test
-        public void signup(){
+      @Test
+//      public void signup(){
+//         WebElement signupLoginBtn = driver.findElement(By.linkText("Signup / Login"));
+//            signupLoginBtn.click();
+//
+//            WebElement nameField = driver.findElement(By.name("name"));
+//            nameField.sendKeys("Mohamed");
+//
+//            WebElement email = driver.findElement(By.xpath("//input[@data-qa='signup-email']"));
+//            email.sendKeys("hassan@atos.net");
+//
+//            WebElement signupBtn = driver.findElement(By.xpath("//button[text()='Signup']"));
+//            signupBtn.click();
+//        }
+
+        public void login(){ //menna
             WebElement signupLoginBtn = driver.findElement(By.linkText("Signup / Login"));
             signupLoginBtn.click();
 
-            WebElement nameField = driver.findElement(By.name("name"));
-            nameField.sendKeys("Mohamed");
+            WebElement email = driver.findElement(By.xpath("//input[@data-qa='login-email']"));
+            email.sendKeys("mennaelnadyy@gmail.com");
 
-            WebElement email = driver.findElement(By.xpath("//input[@data-qa='signup-email']"));
-            email.sendKeys("hassan@atos.net");
+            WebElement passField = driver.findElement(By.name("password"));
+            passField.sendKeys("menna1234");
 
-            WebElement signupBtn = driver.findElement(By.xpath("//button[text()='Signup']"));
-            signupBtn.click();
+            WebElement loginBtn = driver.findElement(By.xpath("//button[@data-qa='login-button']"));
+            loginBtn.click();
+
+            WebElement loggedInMessage = driver.findElement(By.xpath("//li[normalize-space()='Logged in as menna']"));
+            String actualMessage = loggedInMessage.getText();
+            String expectedMessage = "Logged in as menna";
+
+            Assert.assertEquals(actualMessage, expectedMessage, "Login message did not match!");
+
+
         }
 
 
